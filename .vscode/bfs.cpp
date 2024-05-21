b0b07bd9daea3e5c8bf64a26d53a987bcc642ce2
DIECINIUM <saliksaifiin@gmail.com>
2024-05-21 12:57:14 +0530

    adding new file in vscode

---
+++ .vscode/bfs.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+struct node{
+        struct node* p ;
+        int data ;
+        
+
+    };
+int main(){
+     int n ;
+     cin>> n  ;
+     // write bfs for tree  
+     return 0 ;
+}
